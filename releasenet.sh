d45c6c047cf9d453b3fda426db14b699648e35ea
zwj4031 <dnyl@qq.com>
2020-03-16 21:55:08 +0800

    Update release.yml

--- releasenet.sh
+++ releasenet.sh
@@ -5,4 +5,14 @@ then
 fi
 tar -zcvf grub.tar.gz tftpboot
 cp grub.tar.gz ../../netgrubfm.tar.gz
+
+mkdir releases
+    cp grubfm.iso releases/
+    cp grubfm*.efi releases/
+    cp loadfm releases/
+    cd releases
+    7z a ../grubfm-zh_CN.7z *
+    cd ..
+    rm -r releases
+cp grubfm-zh_CN.7z ../../grubfm-zh_CN.7z
 ls ../../
