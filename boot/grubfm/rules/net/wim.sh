8f02c7bab86579328a5d477665ad03469f88fded
zwj4031 <dnyl@qq.com>
2020-05-01 10:32:27 +0800

    update wim.sh

--- boot/grubfm/rules/net/wim.sh
+++ boot/grubfm/rules/net/wim.sh
@@ -1,4 +1,18 @@
+
+source ${prefix}/func.sh;
+
+function auto_swap {
+  if regexp '^hd[0-9a-zA-Z,]+$' ${grubfm_disk};
+  then
+    regexp -s devnum '^hd([0-9]+).*$' ${grubfm_disk};
+    if test "devnum" != "0";
+    then
+      drivemap -s (hd0) (${grubfm_disk});
+    fi;
+  fi;
+}
 set lang=en_US;
+
 terminal_output console;
 loopback wimboot ${prefix}/wimboot.gz;
 if [ "$grub_platform" = "efi" ];
@@ -22,6 +36,12 @@ then
             newc:bcd:(wimboot)/bcd \
             newc:boot.sdi:(wimboot)/boot.sdi \
             newc:boot.wim:"${grubfm_file}";
+
+    auto_swap;
+    set gfxmode=1920x1080,1366x768,1024x768,800x600,auto;
+    terminal_output gfxterm;
+    boot;
+	
 elif [ "$grub_platform" = "pc" ];
 then
   set enable_progress_indicator=1;
