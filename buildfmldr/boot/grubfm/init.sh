fa90256d707e31063f152ea03d18a1caa88ccbe0
zwj4031 <dnyl@qq.com>
2020-05-26 16:34:44 +0800

    add mkfmldr

---
+++ buildfmldr/boot/grubfm/init.sh
@@ -0,0 +1,85 @@
+# Grub2-FileManager
+# Copyright (C) 2016,2017,2018,2019,2020  A1ive.
+#
+# Grub2-FileManager is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Grub2-FileManager is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Grub2-FileManager.  If not, see <http://www.gnu.org/licenses/>.
+
+set pager=0;
+cat --set=modlist ${prefix}/insmod.lst;
+for module in ${modlist};
+do
+    insmod ${module};
+done;
+export enable_progress_indicator=0;
+export grub_secureboot="Not available";
+if [ "${grub_platform}" = "efi" ];
+then
+    search -s -f -q /efi/microsoft/boot/bootmgfw.efi;
+    if [ "${grub_cpu}" = "i386" ];
+    then
+        search -s -f -q /efi/boot/bootia32.efi;
+    else
+        search -s -f -q /efi/boot/bootx64.efi;
+    fi;
+    getenv -t uint8 SecureBoot grub_secureboot;
+    if [ "${grub_secureboot}" = "1" ];
+    then
+        export grub_secureboot="Enabled";
+        sbpolicy -i;
+        fucksb -i;
+        fucksb --off;
+    fi;
+    if [ "${grub_secureboot}" = "0" ];
+    then
+        export grub_secureboot="Disabled";
+    fi;
+    # enable mouse/touchpad
+    # terminal_input --append mouse;
+else
+    search -s -f -q /fmldr;
+fi;
+
+if cpuid -l;
+then
+  export CPU=64;
+else
+  export CPU=32;
+fi
+stat -r -q -s RAM;
+
+loadfont ${prefix}/fonts/unicode.xz;
+
+export locale_dir=${prefix}/locale;
+export secondary_locale_dir=${prefix}/locale/fm;
+
+source ${prefix}/lang.sh;
+
+export grub_disable_esc="1";
+export gfxmode=1024x768;
+export gfxpayload=keep;
+export timeout=3;
+terminal_output gfxterm;
+set color_normal=white/black;
+set color_highlight=black/white;
+export theme_std=${prefix}/themes/StylishDark/theme.txt;
+export theme=${theme_std};
+search --set=efiwin -f -q /efi/boot/bootx64.efi;
+search --set=efigrubfm -f -q /efi/boot/grubfmx64.efi;
+
+menuentry "启动 windows" --class windows --class os {
+	chainloader (${efiwin})/efi/boot/bootx64.efi
+}                               
+menuentry "启动Grub2文件管理器" --class fedora --class os {
+chainloader (${efigrubfm})/efi/boot/grubfmx64.efi
+}       
+
---
+++ mkfmldr.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+echo "x86_64-efi"
+mkdir buildfmldr/boot/grubfm/x86_64-efi
+cd buildfmldr
+find ./boot | cpio -o -H newc > ./memdisk.cpio
+cd ..
+modules=$(cat arch/x64-pxe/fmldr.lst)
+#modules=acpi affs afs all_video at_keyboard bfs bitmap bitmap_scale blocklist boot bsd btrfs cat chain cmp commandline configfile cpio crc date datehook disk dm_nv echo efi_gop efi_mouse efi_uga efifwsetup eval exfat expr ext2 extcmd f2fs fat fatfs fb file fixmmap font fshelp gcry_arcfour gcry_blowfish gcry_camellia gcry_cast5 gcry_crc gcry_des gcry_dsa gcry_idea gcry_md4 gcry_md5 gcry_rfc2268 gcry_rijndael gcry_rmd160 gcry_rsa gcry_seed gcry_serpent gcry_sha1 gcry_sha256 gcry_sha512 gcry_tiger gcry_twofish gcry_whirlpool getargs getenv getkey gfxmenu gfxterm gfxterm_background gfxterm_menu gptsync gzio halt hashsum help hexdump hfs hfsplus http iso9660 jfs jpeg keylayouts keystatus legacycfg linux linuxefi loadbios loadenv loopback ls lsacpi lua lvm lzopio map mdraid09 mdraid09_be memdisk minicmd msdospart multiboot multiboot2 newc normal ntfs ntfscomp part_apple part_bsd part_gpt part_msdos parttool png probe progress raid5rec raid6rec random read reboot regexp reiserfs sbpolicy search setenv setkey sleep squash4 stat syslinuxcfg tar terminal terminfo test tga time tr true udf version video video_colors video_fb videoinfo xfs xnu xzio zfs
+grub-mkimage -m ./buildfmldr/memdisk.cpio -d ./grub/x86_64-efi -p "(memdisk)/boot/grubfm" -c arch/x64-pxe/fmldr.cfg -o fmldr.efi -O x86_64-efi $modules
